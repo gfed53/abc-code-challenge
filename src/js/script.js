10de94a3824d91cc4a02991c8fd1b5e9ff8173d0
gfed53 <gfederico7153@gmail.com>
2016-07-16 17:37:29 -0400

    Arrow link smoothly scrolls downward

--- src/js/script.js
+++ src/js/script.js
@@ -52,4 +52,22 @@ $(function(){
 			}
 		}
 	});
+
+	$('.scroll-arrow').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
+			|| location.hostname == this.hostname) {
+
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
 });
+
+
